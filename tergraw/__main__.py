1d00aea405c954df5acbc5e7d5bf4adc67296460
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2016-03-21 12:11:24 +0100

    main: example of construction printing

--- tergraw/__main__.py
+++ tergraw/__main__.py
@@ -2,11 +2,18 @@
 Tergraw usage exemple
 
 """
+import time
 import tergraw
 
 
 GRAPH = {'a': {'b', 'c'}, 'd': {'b', 'e'}, 'f': {'g'}}
 
+
+for frame in tergraw.pretty_view(GRAPH, oriented=True, construction=True):
+    print(chr(27) + "[2J")
+    print('\n'.join(frame))
+    print('↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥ CONSTRUCTION ↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥↥')
+    time.sleep(0.1)
 
 print('↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧ ORIENTED ↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧↧')
 print('\n'.join(tergraw.pretty_view(GRAPH, oriented=True)))
